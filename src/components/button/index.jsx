a0656a66bb311e594f4c2df645f4956569ee42d7
Dmytro <dbaturin02@gmail.com>
2023-11-30 22:50:15 +0100

    a href fix

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,14 @@
 import styles from "./index.module.scss"
 
 export const Button = ({text = 'Click', style, img, link}) => {
+    const onClick = (e) => {
+        if(link){
+            return 0
+        }
+        e.preventDefault();
+    }
     return(
-        <a target='_blank' href={link}>
+        <a target='_blank' onClick={(e) => onClick(e)} href={link}>
        <button style={style} className={styles.root}>
            {img ? <img style={
                {
