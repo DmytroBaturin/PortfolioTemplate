0665c63b4d1870ac18aeff752e5d0f3b3e367d97
Dmytro <dbaturin02@gmail.com>
2023-12-01 17:16:14 +0100

    minifix

--- src/components/selection/index.jsx
+++ src/components/selection/index.jsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 import styles from './index.module.scss';
 
-export const Selection = forwardRef(({ img, name}, ref) => (
+export const Selection = forwardRef(({img, name}, ref) => (
     <a target='_blank'>
     <div ref={ref} className={styles.root}>
         <div className={styles.container}>
